c19911d79fd923ae5f614cd530c0eda25d26a28a
Matt Terry <matt.terry@gmail.com>
2013-07-06 17:22:54 -0700

    publishconf uses gh-pages url

--- publishconf.py
+++ publishconf.py
@@ -10,8 +10,8 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://www.numfocus.org'
-RELATIVE_URLS = True
+SITEURL = 'http://numfocus.github.io/numfocus.org'
+RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
